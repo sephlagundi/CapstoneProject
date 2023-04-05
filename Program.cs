edeca4a91793f609354a818f70855a752dfbeb8a
joseph lagundi <sephmarchadesch@gmail.com>
2023-04-05 12:55:14 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using EMSwebapp.Data;
+using EMSwebapp.Models;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext <ApplicationDbContext>();
+
+//configs identity frameworks
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 8 ;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+});
+    
+
+builder.Services.AddScoped<ApplicationDbContext, ApplicationDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.Automigrate();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//enables identity EF for LOG IN, REGISTER
+app.UseAuthentication();
+
+app.UseAuthorization(); // process roles and claim
+//has inbuilt object of identity framwork [Principal] - who is the user and what is his role and claims. it will be automatically populated
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
